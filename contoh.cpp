f278839278158bb3e9ac0965d38b0b416957bf3b
kumarayana <84946101+KuMarayana@users.noreply.github.com>
2022-05-13 23:02:14 +0800

    Create contoh.cpp

---
+++ contoh.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void FranceNumber() {
+     int Number[5] = {2, 3, 6, 9, 15};
+     int Number2[5] = {3, 7, 10, 13, 20};
+
+     for (int i = 0; i <= Number; i++) {
+         for (int j = 0;j <= Number2; j++) {
+             if (i[5] < 12 && j[5] > 13) {
+                 cout<<i<<" "<<j<<endl;
+             }
+         }
+     }
+}
+
